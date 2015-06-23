7e3079ebdcde8dfd721cdc7b4cab8367b6a05eda
darkkaos505 <callum.waddell@btinternet.com>
2015-06-23 23:06:47 +0100

    Arrow Move
    First pass of moving object using mouse

---
+++ TacticsVIewer/Assets/Custom Assets/Scripts/MouseManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseManager : MonoBehaviour {
+
+	// Use this for initialization
+
+
+    ObjectAxisMove objectAxisMove;
+
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+
+        HandelClicks();
+
+
+    }
+
+    void HandelClicks()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray , out hitInfo)) 
+            {                
+                if( hitInfo.collider.CompareTag("MoveArrow") )
+                {
+                    objectAxisMove = hitInfo.collider.gameObject.GetComponent<ObjectAxisMove>();
+                    objectAxisMove.StartMove();
+                }
+            }
+        }
+
+
+        if(Input.GetMouseButtonUp(0) )
+        {
+
+            if(objectAxisMove != null)
+            {
+                objectAxisMove.StopMove();
+                objectAxisMove = null;     
+            }
+        }
+
+
+    }
+}
---
+++ TacticsVIewer/Assets/Custom Assets/Scripts/ObjectAxisMove.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectAxisMove : MonoBehaviour
+{
+
+    public Transform ObjectToMove;
+
+
+    enum MoveStateEnum { stoped , moving };
+
+    MoveStateEnum moveState = MoveStateEnum.stoped;
+
+    float horizontalSpeed = 1;
+    float verticalSpeed = 1;
+
+
+    public Vector3 direction = Vector3.forward;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (moveState == MoveStateEnum.moving)
+        {
+            float h = horizontalSpeed * Input.GetAxis("Mouse X");
+            float v = verticalSpeed * Input.GetAxis("Mouse Y");
+
+            ObjectToMove.Translate(direction * h);
+        }
+    }
+
+
+    public void StartMove()
+    {
+        moveState = MoveStateEnum.moving;
+    }
+
+    public void StopMove()
+    {
+        moveState = MoveStateEnum.stoped;
+    }
+
+}
